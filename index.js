14d5255484c716b1e6269e3b8c716bc42c2378f1
hillrg22 <hillrg22@gmail.com>
2018-12-04 10:47:04 -0700

    installed cors and bodyparser. added basic get route

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3002
+const parser = require('body-parser')
+const cors = require('cors')
 
 
+app.use(parser.urlencoded({extended:false}))
+app.use(parser.json())
+
+app.get('/',(req,res,next)=>{
+  res.send('got it 🏈')
+})
 
 app.listen(port, () =>{
   console.log(`Running on port ${port}`)
