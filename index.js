95bf53a8aa59dedb98af00ac017d4811f5983071
hillrg22 <hillrg22@gmail.com>
2018-12-04 11:12:56 -0700

    set up knexfile, installed pg package, created some local test data

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const port = 3002
 const parser = require('body-parser')
 const cors = require('cors')
 
-
 app.use(parser.urlencoded({extended:false}))
 app.use(parser.json())
 
+const books = require('./books.js')
+const test = require('./test.js')
+
+
+
+
 app.get('/',(req,res,next)=>{
   res.send('got it 🏈')
+})
+
+
+app.get('/books',(req,res,next)=> {
+  res.json({books})
 })
 
 app.listen(port, () =>{
---
+++ test.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    "name": "rob",
+    "age": 28
+  },
+  {
+    "name": "ian",
+    "age": 25
+  }
+]
