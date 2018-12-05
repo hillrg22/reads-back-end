febff8c0e9e0182b8e1e963f747d8cbeebc733a7
hillrg22 <hillrg22@gmail.com>
2018-12-05 16:30:08 -0700

    minor changes to routes

--- Routes/author_bookRoute.js
+++ Routes/author_bookRoute.js
@@ -3,45 +3,6 @@ const router = express.Router()
 const knex = require('../db/connection.js')
 const reformatBooks = require('../db/reformatBooks.js')
 
-// reformatBooks = (books) => {
-//
-//   const reformatted = []
-//   const booksById = {}
-//
-//   books.forEach(book => {
-//     const currentBookId = book.book_id
-//
-//
-//     if (booksById[currentBookId]) {
-//       booksById[book.book_id].authors.push({
-//         author_id: book.author_id,
-//         first_name: book.first_name,
-//         last_name: book.last_name
-//       })
-//     } else {
-//       booksById[currentBookId] = {
-//         book_id: book.book_id,
-//         book_title: book.title,
-//         book_genre: book.genre,
-//         book_description: book.book_description,
-//         book_cover_url: book.cover_url,
-//
-//         authors: [{
-//           author_id: book.author_id,
-//           first_name: book.first_name,
-//           last_name: book.last_name,
-//           bio: book.author_bio,
-//           portrait_url: book.author_portrait_url,
-//
-//         }]
-//       }
-//       reformatted.push(booksById[currentBookId])
-//     }
-//   })
-//   return reformatted
-// }
-
-
 
 
 router.get('/', (req,res,next) =>{
